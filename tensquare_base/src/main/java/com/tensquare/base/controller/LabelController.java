fac790c8dcb449d79cf3948e8f4f4429ac4b54cb
Mr <120073768@qq.com>
2019-09-15 09:41:35 +0800

    初始微服务

---
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -0,0 +1,78 @@
+package com.tensquare.base.controller;
+
+import com.tensquare.base.pojo.Label;
+import com.tensquare.base.service.LabelService;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+/**
+ * 标签控制层
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/label")
+public class LabelController {
+
+    @Autowired
+    private LabelService labelService;
+
+    /**
+     * 查询全部列表
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll(){
+        List<Label> list = labelService.findAll();
+        return new Result(true, StatusCode.OK,"查询成功",list);
+    }
+
+    /**
+     * 根据ID查询标签
+     * @return
+     */
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Result findById(@PathVariable String id){
+        return new Result(true,StatusCode.OK,"查询成功",labelService.findById(id));
+    }
+
+    /**
+     * 增加标签
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public Result add(@RequestBody Label label){
+        labelService.add(label);
+        return new Result(true,StatusCode.OK,"增加成功");
+    }
+
+    /**
+     * 修改标签
+     * @return
+     */
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public Result update(@RequestBody Label label, @PathVariable String id){
+        label.setId(id);
+        labelService.update(label);
+        return new Result(true,StatusCode.OK,"修改成功");
+    }
+
+    /**
+     * 删除标签
+     * @return
+     */
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String id){
+        labelService.deleteById(id);
+        return new Result(true,StatusCode.OK,"删除成功");
+    }
+
+}
