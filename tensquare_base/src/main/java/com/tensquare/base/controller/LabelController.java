59fb935731c00b625963de93485a164ce36e8fed
Mr <120073768@qq.com>
2019-09-15 21:15:59 +0800

    分页条件查询

--- tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -2,9 +2,11 @@ package com.tensquare.base.controller;
 
 import com.tensquare.base.pojo.Label;
 import com.tensquare.base.service.LabelService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -73,6 +75,26 @@ public class LabelController {
     public Result deleteById(@PathVariable String id){
         labelService.deleteById(id);
         return new Result(true,StatusCode.OK,"删除成功");
+    }
+
+    /**
+     * 条件查询
+     * @return
+     */
+    @RequestMapping(value = "/search",method = RequestMethod.POST)
+    public Result findSearch(@RequestBody Label label){
+        List<Label> list = labelService.findSearch(label);
+        return new Result(true, StatusCode.OK,"查询成功",list);
+    }
+
+    /**
+     * 条件分页查询
+     * @return
+     */
+    @RequestMapping(value = "/search/{page}/{size}",method = RequestMethod.POST)
+    public Result pageQuery(@RequestBody Label label,@PathVariable int page,@PathVariable int size){
+        Page<Label> pageData = labelService.pageQuery(label,page,size);
+        return new Result(true, StatusCode.OK,"查询成功",new PageResult<Label>(pageData.getTotalElements(),pageData.getContent()));
     }
 
 }
--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -4,9 +4,18 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import util.IdWorker;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -60,5 +69,65 @@ public class LabelService {
      */
     public void deleteById(String id){
         labelDao.deleteById(id);
+    }
+
+    /**
+     * 条件查询
+     * @return
+     */
+    public List<Label> findSearch(Label label) {
+        return labelDao.findAll(new Specification<Label>() {
+            /**
+             * @param root 根对象，用来封装条件
+             * @param query  封装查询的关键字 如：group by ，order by等
+             * @param cb   用来封装跟对象，如果为null，表示不需要任何条件
+             * @return
+             */
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                //new 一个集合，存放所有条件
+                List<Predicate> list = new ArrayList<>();
+                if(label.getLabelname() != null && !"".equals(label.getLabelname())){
+                    Predicate labelname = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");//where labelname like "%哈%"
+                    list.add(labelname);
+                }
+                if(label.getState() != null && !"".equals(label.getState())){
+                    Predicate state = cb.like(root.get("state").as(String.class), label.getState());//where state = "1"
+                    list.add(state);
+                }
+                Predicate[] prr = new Predicate[list.size()];
+                prr = list.toArray(prr);
+                return cb.and(prr);
+            }
+        });
+    }
+
+    /**
+     * 条件分页查询
+     * @param label
+     * @param page
+     * @param size
+     * @return
+     */
+    public Page<Label> pageQuery(Label label, int page, int size) {
+        Pageable pageable = PageRequest.of(page-1,size);
+        return labelDao.findAll(new Specification<Label>() {
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                //new 一个集合，存放所有条件
+                List<Predicate> list = new ArrayList<>();
+                if(label.getLabelname() != null && !"".equals(label.getLabelname())){
+                    Predicate labelname = cb.like(root.get("labelname").as(String.class), "%" + label.getLabelname() + "%");//where labelname like "%哈%"
+                    list.add(labelname);
+                }
+                if(label.getState() != null && !"".equals(label.getState())){
+                    Predicate state = cb.like(root.get("state").as(String.class), label.getState());//where state = "1"
+                    list.add(state);
+                }
+                Predicate[] prr = new Predicate[list.size()];
+                prr = list.toArray(prr);
+                return cb.and(prr);
+            }
+        },pageable);
     }
 }
